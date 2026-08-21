6a42bd8982846501f24463990aaace062145e1e3
erzetkayre <erzetkayre@gmail.com>
2026-08-21 10:33:52 +0700

    fix: refactor course components

--- components/course/BipaCourse.tsx
+++ components/course/BipaCourse.tsx
@@ -1,6 +1,5 @@
-import { Reveal } from "@/components/ui/Reveal";
 import { DocTable } from "@/components/docs/DocTable";
-import { CourseHeading } from "@/components/course/CourseHeading";
+import { CurriculumSection } from "@/components/course/CurriculumSection";
 import { getCourseGuide } from "@/lib/constants/course-guide";
 import { getDictionary } from "@/lib/i18n/get-dictionary";
 import type { Locale } from "@/lib/i18n/locales";
@@ -11,13 +10,14 @@ export async function BipaCourse({ locale }: { locale: Locale }) {
   const { bipaSyllabus } = getCourseGuide(locale);
 
   return (
-    <div id="curriculum-bipa" className="flex flex-col gap-6 scroll-mt-44 lg:scroll-mt-32">
-      <CourseHeading number="2" title={t.title} />
-      <Reveal className="flex flex-col gap-4 text-sm text-black/70 md:text-base">
-        <p>{t.paragraph}</p>
-        <p className="text-xs text-black/45">{t.footnote}</p>
-      </Reveal>
+    <CurriculumSection
+      id="curriculum-bipa"
+      number="2"
+      title={t.title}
+      paragraph={t.paragraph}
+      paragraphExtra={<p className="text-xs text-black/45">{t.footnote}</p>}
+    >
       <DocTable headers={t.tableHeaders} rows={bipaSyllabus} />
-    </div>
+    </CurriculumSection>
   );
 }
